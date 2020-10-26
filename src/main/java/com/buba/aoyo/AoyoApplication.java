e25402b1f8e23012f66cee608ca87cc4dbe665fa
changjiang <1161268455@qq.com>
2020-10-26 15:37:00 +0800

    new project

---
+++ src/main/java/com/buba/aoyo/AoyoApplication.java
@@ -0,0 +1,13 @@
+package com.buba.aoyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AoyoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buba/aoyo/AoyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buba.aoyo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
