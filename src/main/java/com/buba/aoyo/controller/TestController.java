247a92aaaf26be0d32037d05f0a42ad9c0a6d4c2
demo <2041034701@qq.com>
2020-10-26 16:25:19 +0800

    tianjiatest1

--- src/main/java/com/buba/aoyo/controller/TestController.java
+++ src/main/java/com/buba/aoyo/controller/TestController.java
@@ -3,5 +3,6 @@ package com.buba.aoyo.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("xiaojiba");
+        System.out.println("xiaojiba");
     }
 }
